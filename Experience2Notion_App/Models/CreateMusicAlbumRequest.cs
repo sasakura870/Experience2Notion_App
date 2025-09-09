257f68facacee9178c99f3c58a4eb038aa9ee997
笹倉直哉 <a_20100409@yahoo.co.jp>
2025-09-09 22:33:32 +0900

    音楽アルバムのページ作成リクエストを表すクラスを追加

---
+++ Experience2Notion_App/Models/CreateMusicAlbumRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Experience2Notion_App.Models;
+internal class CreateMusicAlbumRequest
+{
+    [JsonPropertyName("title")]
+    public string Title { get; set; } = string.Empty;
+
+    [JsonPropertyName("artist")]
+    public string Artist { get; set; } = string.Empty;
+}
